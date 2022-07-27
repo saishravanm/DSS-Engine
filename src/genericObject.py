417d037bf1a5ec79d85926adc129e057a5e075e0
Stepan Antonyuk <stepan.antonyuk@gmail.com>
2022-07-26 19:34:42 -0500

    fix: render related parameters are in renderParamas now a class that contains all important render related parameters

--- src/genericObject.py
+++ src/genericObject.py
@@ -1,21 +1,13 @@
-import pygame
-import math
-import cmath
 import shape as sh
+import renderParams as rp
 
 
 class GenericObject():
     def __init__(self, pos, shape=sh.Rect(100, 100)):
         self.pos = pos
         self.shape = shape  # type of shape square or circle
-        self.color = (0, 250, 0)
-        # self.width = 100  # it is contained in shape class labeled as sh
-        # self.height = 100  # it is contained in shape class labeled as sh
-        self.thickness = 0
-        self.curve = 0
-        self.draw_map = {"rect":self.draw_rec}
-        self.define_hit_box_map = {"rect":self.define_rec()}
-        self.look = self.define_hit_box_map[self.shape.shape_type]  # is it rectangle hit-box or circle hit-box
+        self.render_params_map = {"rect":rp.Rect, "circle":rp.Circle}
+        self.render_params = self.render_params_map[self.shape.shape_type]
 
     def get_shape(self):
         return self.shape
@@ -23,29 +15,13 @@ class GenericObject():
     def get_pos(self):
         return self.pos
 
-    def define_rec(self):
-        return pygame.Rect(self.pos[0], self.pos[1], self.shape.width, self.shape.height)
-
-    # def draw(self, screen_pos, screen):
-    #     self.draw_map[self.shape](screen_pos, screen)
-    #
-    # def draw_rec(self, screen_pos, screen):
-    #     pygame.draw.rect(screen, self.color, (screen_pos[0], screen_pos[1], self.width, self.height), self.thickness, self.curve)
-
-    def draw(self, converter, screen):
-        self.draw_map[self.shape.shape_type](converter, screen)
-
-    def draw_rec(self, converter, screen):
-        screen_pos = converter.from_universe(self.pos)
-        pygame.draw.rect(screen, self.color, (screen_pos[0], screen_pos[1], self.shape.width, self.shape.height), self.thickness, self.curve)
-
 
 class Static(GenericObject):
     def __init__(self, pos, shape=sh.Rect(100, 100)):
         super(Static, self).__init__(pos, shape)
 
     def update(self, tiles, converter, screen):
-        self.draw(converter, screen)
+        pass
 
 # class CollidableStatic(Static):
 #     def __init__(self, pos, shape):
@@ -89,7 +65,6 @@ class Kinetic(GenericObject):
 
     def update(self, tiles, converter, screen):  # junk
         self.move()
-        self.draw(converter, screen)
 
 
 class CollidableKinetic(Kinetic):
@@ -98,37 +73,9 @@ class CollidableKinetic(Kinetic):
         self.collision_map = {"Static":True}
         # self.collision_check_map = {"rec": self.rec_collision_checker}
 
-    def collision_test(self, rect, tiles):
-        collisions = []
-        for tile in tiles:
-            if tile != self and rect.colliderect(tile.look):
-                if self.collision_map[type(tile).__name__]:
-                    collisions.append(tile)
-        return collisions
-
-    def collision_checker(self, rect, movement, tiles):
-        rect.x += movement[0]
-        collisions = self.collision_test(rect, tiles)
-        for tile in collisions:
-            if movement[0] > 0:
-                rect.right = tile.look.left
-            if movement[0] < 0:
-                rect.left = tile.look.right
-        rect.y += movement[1]
-        collisions = self.collision_test(rect, tiles)
-        for tile in collisions:
-            if movement[1] > 0:
-                rect.bottom = tile.look.top
-            if movement[1] < 0:
-                rect.top = tile.look.bottom
-        return rect
-
     def update(self, tiles, converter, screen):
-        # new_look = self.collision_check_map[self.shape](self.look, self.speed_xy, tiles)
         if self.direction != (0, 0):
-            new_look = self.collision_checker(self.look, self.direction, tiles)
-            self.pos = (new_look.x, new_look.y)
-        self.draw(converter, screen)
+            pass
 
 
 # class NonCollidableKinetic(Kinetic):
