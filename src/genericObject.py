30429a46d2e8171cdb6149cbf77e50a529d13259
Stepan Antonyuk <stepan.antonyuk@gmail.com>
2022-07-26 19:46:50 -0500

    chore: some namings and implementations were fixed

--- src/genericObject.py
+++ src/genericObject.py
@@ -41,24 +41,24 @@ class Static(GenericObject):
 
 
 class Kinetic(GenericObject):
-    def __init__(self, direction, speed, pos, shape=sh.Rect(100, 100)):
+    def __init__(self, speed_delta, speed, pos, shape=sh.Rect(100, 100)):
         super(Kinetic, self).__init__(pos, shape)
-        self.direction = direction
+        self.speed_delta = speed_delta
         self.speed = speed
         self.color = (250, 0, 0)
         self.speed_xy = self.get_speed_xy()
 
     def get_direction(self):
-        return self.direction
+        return self.speed_delta
 
     def get_speed(self):
         return self.speed
 
     def get_speed_xy(self):
-        ratio = abs(self.direction[0]) + abs(self.direction[1])
+        ratio = abs(self.speed_delta[0]) + abs(self.speed_delta[1])
         ratio_speed = (self.speed / ratio)
-        speed_x = ratio_speed * self.direction[0]
-        speed_y = ratio_speed * self.direction[1]
+        speed_x = ratio_speed * self.speed_delta[0]
+        speed_y = ratio_speed * self.speed_delta[1]
         return (speed_x, speed_y)
 
     def move(self):
@@ -73,13 +73,15 @@ class Kinetic(GenericObject):
 
 
 class CollidableKinetic(Kinetic):
+
+    collision_map = {Static}
+
     def __init__(self, direction, speed, pos, shape=sh.Rect(100, 100)):
         super(CollidableKinetic, self).__init__(direction, speed, pos, shape)
-        self.collision_map = {"Static":True}
         # self.collision_check_map = {"rec": self.rec_collision_checker}
 
     def update(self, tiles, converter, screen):
-        if self.direction != (0, 0):
+        if self.speed_delta != (0, 0):
             pass
 
 
